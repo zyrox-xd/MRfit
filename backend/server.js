8dc48b91432a413d5d77cc68bfc9fe88b2174547
Shaarif <1sha2arif2@gmail.com>
2025-08-17 12:40:17 +0530

    mod logo

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ const cors = require("cors");
 const nodemailer = require("nodemailer");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 
 // Middleware
 app.use(bodyParser.json());
